e743562b4033016ebb3f0d2baeefe40622362e70
Bonnie Zhou <bonniez@google.com>
2018-11-08 12:18:28 -0800

    line ripple and typography working

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+import {MDCLineRipple} from '@material/line-ripple';
+
+class Link {
+  constructor(root) {
+    this.root_ = root;
+    this.lineRipple_ = new MDCLineRipple(root.querySelector('.mdc-line-ripple'));
+
+    this.root_.addEventListener('mouseover', () => this.lineRipple_.activate());
+    this.root_.addEventListener('mouseout', () => this.lineRipple_.deactivate());
+  }
+}
+
+const linkElements = document.querySelectorAll('.link');
+linkElements.forEach((linkEl) => {
+  new Link(linkEl);
+});
