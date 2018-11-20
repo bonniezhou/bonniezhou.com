fb51de53cd630b831cda6a7cfc374f9ef847599d
Bonnie Zhou <bonniez@google.com>
2018-11-20 09:32:18 -0800

    fix up css

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class Link {
   }
 }
 
-const linkElements = document.querySelectorAll('.link');
+const linkElements = document.querySelectorAll('.underlined-link');
 linkElements.forEach((linkEl) => {
   new Link(linkEl);
 });
