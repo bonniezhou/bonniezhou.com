261796dce0b34e1d7944b80c82e2a7f8d99eb2fc
Bonnie Zhou <bonniez@google.com>
2018-11-20 10:19:59 -0800

    a11y stuff

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import {MDCLineRipple} from '@material/line-ripple';
 
-class Link {
+class UnderlinedLink {
   constructor(root) {
     this.root_ = root;
     this.lineRipple_ = new MDCLineRipple(root.querySelector('.mdc-line-ripple'));
@@ -9,8 +9,31 @@ class Link {
     this.root_.addEventListener('mouseout', () => this.lineRipple_.deactivate());
   }
 }
+  
+// Workaround until :focus-visible supported in all browsers
+// https://css-tricks.com/keyboard-only-focus-styles/
+class Anchor {
+  constructor(root) {
+    this.root_ = root;
+    this.isClicked_ = false;
+
+    this.root_.addEventListener('focus', () => {
+      if (this.isClicked_) return;
+      this.root_.classList.add('focus-visible');
+    });
+    this.root_.addEventListener('mousedown', () => {
+      this.isClicked_ = true;
+      this.root_.classList.remove('focus-visible');
+    });
+    this.root_.addEventListener('blur', () => {
+      if (this.isClicked_) this.isClicked_ = false;
+      this.root_.classList.remove('focus-visible');
+    });
+  }
+}
 
 const linkElements = document.querySelectorAll('.underlined-link');
-linkElements.forEach((linkEl) => {
-  new Link(linkEl);
-});
+linkElements.forEach((linkEl) => new UnderlinedLink(linkEl));
+
+const anchorElements = document.querySelectorAll('a');
+anchorElements.forEach((a) => new Anchor(a));
